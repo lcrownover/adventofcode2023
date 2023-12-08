3753b031c23424a066ba6a724e22ff1fd79bcc64
Lucas Crownover <lcrownover127@gmail.com>
2023-12-08 10:34:56 -0800

    rewrote using LCM

--- src/days/eight/part2.rs
+++ src/days/eight/part2.rs
@@ -1,3 +1,4 @@
+use primes::{PrimeSet, Sieve};
 use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug, Clone, Copy)]
@@ -89,52 +90,109 @@ fn get_map_lines(lines: &Vec<String>) -> HashMap<String, MapLine> {
     hm
 }
 
-fn check_success(maplines: &VecDeque<MapLine>) -> bool {
-    for ml in maplines.iter() {
-        if !ml.id.ends_with("Z") {
-            return false;
-        }
-    }
-    true
+fn check_success(mapline: &MapLine) -> bool {
+    mapline.id.ends_with("Z")
 }
 
-fn process_direction(
-    directory: &HashMap<String, MapLine>,
-    maplines: &VecDeque<MapLine>,
-    direction: &Direction,
-) -> VecDeque<MapLine> {
-    let mut new_maplines: VecDeque<MapLine> = VecDeque::new();
-    for ml in maplines {
-        match direction {
-            Direction::Left => new_maplines.push_back(directory.get(&ml.left).unwrap().clone()),
-            Direction::Right => new_maplines.push_back(directory.get(&ml.right).unwrap().clone()),
-        }
-    }
-    new_maplines
-}
-
-pub fn run(lines: &Vec<String>) {
-    let instructions_text = get_instructions(lines);
-    let mut instructions: Instructions = Instructions::new(instructions_text);
-    let maplines = get_map_lines(lines);
-
-    let mut counter = 0;
-
-    // get maplines where id ends in 'A'
-    let mut next_maplines: VecDeque<MapLine> = VecDeque::new();
+fn get_starting_maplines(maplines: &HashMap<String, MapLine>) -> Vec<MapLine> {
+    let mut starting: Vec<MapLine> = Vec::new();
     for k in maplines.keys() {
         if k.ends_with("A") {
-            next_maplines.push_back(maplines.get(k).unwrap().clone())
+            starting.push(maplines.get(k).unwrap().clone())
+        }
+    }
+    starting
+}
+
+fn follow_instruction(
+    master_map: &HashMap<String, MapLine>,
+    current_mapline: &MapLine,
+    instruction: &Direction,
+) -> MapLine {
+    match instruction {
+        Direction::Left => return master_map.get(&current_mapline.left).unwrap().clone(),
+        Direction::Right => return master_map.get(&current_mapline.right).unwrap().clone(),
+    }
+}
+
+trait IncrementExt {
+    fn increment(&mut self, key: u64);
+}
+
+impl IncrementExt for HashMap<u64, u64> {
+    fn increment(&mut self, key: u64) {
+        match self.get_mut(&key) {
+            Some(v) => *v += 1,
+            None => {
+                self.insert(key, 1);
+                ()
+            }
+        };
+    }
+}
+
+fn get_prime_factors(num: &u64) -> HashMap<u64, u64> {
+    let mut prime_list = Sieve::new();
+    let mut out = HashMap::new();
+    let mut n = num.clone();
+    for pnum in prime_list.iter() {
+        if n == 1 {
+            break;
+        }
+        loop {
+            if n.rem_euclid(pnum) != 0 {
+                break;
+            }
+            out.increment(pnum.clone());
+            n = n / pnum;
+        }
+    }
+    out
+}
+
+fn lcm(nums: &Vec<u64>) -> u64 {
+    let mut prime_maps: Vec<HashMap<u64, u64>> = vec![];
+    for num in nums {
+        // println!("factorizing num {num}");
+        prime_maps.push(get_prime_factors(num))
+    }
+    // println!("prime maps {prime_maps:?}");
+    // it looks like each number is just two prime numbers multiplied by eachother
+    // so we can just grab all the keys, dedup it, then multiply them together
+    let mut multiples: Vec<u64> = prime_maps
+        .iter()
+        .map(|hm| hm.keys().map(|k| k.clone()).collect::<Vec<u64>>())
+        .flatten()
+        .collect();
+    multiples.sort();
+    multiples.dedup();
+
+    // println!("multiples {multiples:?}");
+    multiples.iter().product()
+}
+
+// for each of the maplines, find how many iterations it takes to get to the corresponding 'Z',
+// then take the LCM of all those values
+pub fn run(lines: &Vec<String>) {
+    let master_map = get_map_lines(lines);
+    let instructions_text = get_instructions(lines);
+    let starting_maplines = get_starting_maplines(&master_map);
+    let mut mapline_cycles: Vec<u64> = vec![];
+
+    for ml in starting_maplines {
+        let instructions: Instructions = Instructions::new(instructions_text);
+        let mut cycle_counter = 0;
+        let mut current_mapline: MapLine = ml.clone();
+        for instruction in instructions {
+            if check_success(&current_mapline) {
+                mapline_cycles.push(cycle_counter);
+                break;
+            }
+            cycle_counter += 1;
+            current_mapline = follow_instruction(&master_map, &current_mapline, &instruction)
         }
     }
 
-    loop {
-        if check_success(&next_maplines) {
-            break;
-        }
-        counter += 1;
-        let direction = instructions.next().unwrap();
-        next_maplines = process_direction(&maplines, &next_maplines, &direction)
-    }
-    println!("{counter}");
+    let ans = lcm(&mapline_cycles);
+    println!("{ans}")
 }
